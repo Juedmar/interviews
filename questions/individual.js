5bb57ef787d8f8f32e5d433a45d12ce5491adca4
Juan Eduardo Méndez Arias <116532250+Juedmar@users.noreply.github.com>
2023-07-19 19:31:33 -0400

    Add files via upload

--- questions/individual.js
+++ questions/individual.js
@@ -3,13 +3,25 @@
 // MAKE SURE TO START AND FINISH WITH QUOTATION SIGNS.
 function myFunctionOne() {
   idArray = new Array()
-  idArray [1] = "<u>NOTICE:</u> There are no tests being carried out today!"
-  idArray [2] = "<u>SAMPLE:</u> How often do you go shopping? What do you usually buy?"
-  idArray [3] = "<u>SAMPLE:</u> What is your favorite day of the week? Why?"
+  idArray [1] = "Practice: What is your full name?"
+  idArray [2] = "Practice: How are you doing today?"
+  idArray [3] = "Practice: What is your favorite color?"
+  idArray [4] = "Practice: What is your e-mail address?"
+  idArray [5] = "Practice: What is your favorite season?"
+  idArray [6] = "Practice: How often do you go shopping?"
+  idArray [7] = "Practice: How do you spell your last name?"
+  idArray [8] = "Practice: What is your favorite food or drink?"
+  idArray [9] = "Practice: What is your favorite day of the week?"
+  idArray [10] = "Practice: What is your favorite month of the year?"
+  idArray [11] = "Practice: What time do you get up in the morning?"
+  idArray [12] = "Practice: What is your favorite sport? Do you practice often?"
+  idArray [13] = "Practice: What do you usually do during long weekeneds or holidays?"
+  idArray [14] = "Practice: Do you have any hobbies? When did you start doing them?"
+  idArray [15] = "Notice: These are practice questions only. There are no interviews today!"
 
 // SET THE NUMBER OF QUESTIONS ON THE SECOND LINE BELOW!
   document.getElementById("selectone").onclick = myFunctionOne;
-  randomParagraph = Math.floor(Math.random()*3);
+  randomParagraph = Math.floor(Math.random()*15);
 
 // THIS LINE DISPLAYS THE RANDOM QUESTIONS [DO NOT CHANGE]!
   document.getElementById("resultone").innerHTML = idArray[randomParagraph + 1];
--- questions/pairwork.js
+++ questions/pairwork.js
@@ -3,16 +3,17 @@
 // MAKE SURE TO START AND FINISH WITH QUOTATION SIGNS.
 function myFunctionPairs() {
   idArray = new Array()
-  idArray [1] = "<u>NOTICE:</u> There are no tests being carried out today!"
-  idArray [2] = "<u>SAMPLE:</u> Talk to each other about your daily routine."
-  idArray [3] = "<u>SAMPLE:</u> Talk to each other about the people in your family."
+  idArray [1] = "Practice: Talk to each other about your favorite foods and drinks."
+  idArray [2] = "Practice: Talk to each other about your daily routine and pastimes."
+  idArray [3] = "Practice: Talk to each other about about your ideal vacation."
+  idArray [4] = "Notice: These are practice questions only. There are no interviews today!"
 
 // SET THE NUMBER OF QUESTIONS ON THE SECOND LINE BELOW!
   document.getElementById("selectpairs").onclick = myFunctionPairs;
-  randomParagraph = Math.floor(Math.random()*3);
+  randomParagraph = Math.floor(Math.random()*4);
 
 // THIS LINE DISPLAYS THE RANDOM QUESTIONS [DO NOT CHANGE]!
-  document.getElementById("resultpairs").innerHTML = idArray[randomParagraph + 1];
+  document.getElementById("resultpairs").innerHTML = idArray[randomParagraph + 1];       
 
 // THIS LINE HIDES THE TEXT OF INDIVIDUAL TOPICS [DO NOT CHANGE]!
   document.getElementById('resultone').style.color = '#FFFF44';
