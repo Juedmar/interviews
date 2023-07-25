b4b75f8194041cb154c0de29e219f0062709e279
Juan Eduardo Méndez Arias <116532250+Juedmar@users.noreply.github.com>
2023-07-25 17:17:59 -0400

    Add files via upload

--- questions/individual.js
+++ questions/individual.js
@@ -3,25 +3,25 @@
 // MAKE SURE TO START AND FINISH WITH QUOTATION SIGNS.
 function myFunctionOne() {
   idArray = new Array()
-  idArray [1] = "<u>What's a typical Saturday for you?</u> Describe what you generally do and what you don't do on this day."
-  idArray [2] = "Talk about all <u>the things that you are doing this year</u>. Mention things about your family or personal life, your education, sports, music or hobbies, etc."
-  idArray [3] = "<u>What do you usually do on Friday nights?</u> Talk about what you usually do at the end of the week, including the activities, frequency, times, locations, etc."
-  idArray [4] = "Think about someone you know well and <u>describe this person, including their physical appearance, their personality</u> and a little bit of their daily routine."
-  idArray [5] = "<u>Describe your ideal romantic partner</u>. Talk about his/her appearance, personality, why you would like them and the things he or she normally does."
-  idArray [6] = "How many people are there in your family? <u>Describe at least 2 of them, including their physical appearance, their personality</u> and a little bit of their daily routine."
-  idArray [7] = "Who is your closest or favorite family member? Why? <u>Describe him/her in about 1 minute</u>, including some of the things they normally do."
-  idArray [8] = "Talk about any <u>hobbies, sports or healthy activities you have practiced</u>. Try to fully describe your experiences, including every detail."
-  idArray [9] = "Talk about <u>how you first met</u> one of your friends or classmates. You should say <u>when you met</u>, where you met, what you and your friend <u>were doing</u> at that time."
-  idArray [10] = "Talk about one activity using technology you <u>have been doing recently</u> (surfing the internet, watching TV, using social media etc). You should explain <u>why you have been using it</u>."
-  idArray [11] = "Talk about the <u>last time you went out</u> with a friend or family member. You should say <u>where you went, what you did</u> and why."
-  idArray [12] = "Talk about the <u>last time you went on holiday</u>. You should say <u>who you went with, where you went, what you did</u> and if you enjoyed it."
-
-  idArray [13] = "🗣️ Talk about an <u>interesting / funny anecdote that you had</u> in the past. You should describe the situation, how you felt and why you felt that way."
-  idArray [14] = "🗣️ Talk about <u>a movie or book that you liked</u>. You should describe the movie or book and say why you liked it."
+  idArray [1] = "Practice: What is your full name?"
+  idArray [2] = "Practice: How are you doing today?"
+  idArray [3] = "Practice: What is your favorite color?"
+  idArray [4] = "Practice: What is your e-mail address?"
+  idArray [5] = "Practice: What is your favorite season?"
+  idArray [6] = "Practice: How often do you go shopping?"
+  idArray [7] = "Practice: How do you spell your last name?"
+  idArray [8] = "Practice: What is your favorite food or drink?"
+  idArray [9] = "Practice: What is your favorite day of the week?"
+  idArray [10] = "Practice: What is your favorite month of the year?"
+  idArray [11] = "Practice: What time do you get up in the morning?"
+  idArray [12] = "Practice: What is your favorite sport? Do you practice often?"
+  idArray [13] = "Practice: What do you usually do during long weekeneds or holidays?"
+  idArray [14] = "Practice: Do you have any hobbies? When did you start doing them?"
+  idArray [15] = "Notice: These are practice questions only. There are no interviews today!"
 
 // SET THE NUMBER OF QUESTIONS ON THE SECOND LINE BELOW!
   document.getElementById("selectone").onclick = myFunctionOne;
-  randomParagraph = Math.floor(Math.random()*14);
+  randomParagraph = Math.floor(Math.random()*15);
 
 // THIS LINE DISPLAYS THE RANDOM QUESTIONS [DO NOT CHANGE]!
   document.getElementById("resultone").innerHTML = idArray[randomParagraph + 1];
