4b65fb81a027359d2075263113fdb1f3a759c13a
Juan Eduardo Méndez Arias <116532250+Juedmar@users.noreply.github.com>
2023-08-29 23:22:18 -0400

    Add files via upload

--- questions/individual.js
+++ questions/individual.js
@@ -3,19 +3,31 @@
 // MAKE SURE TO START AND FINISH WITH QUOTATION SIGNS.
 function myFunctionOne() {
   idArray = new Array()
-  idArray [1] = "Think about someone you know well and <u>describe this person, including their physical appearance, their personality</u> and a little bit of their daily routine."
-  idArray [2] = "<u>Describe your favorite sports / pastime activities you like doing</u>. Talk about the activities, the frequency, the gear, the locations and people with whom you do them."
-  idArray [3] = "<u>Describe your favorite holiday celebration</u>. Talk about the event and the why you celebrate it, the things you do, if you visit a special locations, the food & drinks, the music, etc."
-  idArray [4] = "Think about two people in your family. <u>Describe and compare all their different characteristics</u>, including physical features, personalities and routines, etc… including how they are different from each other."
+
+// UNIT 2:
+
+  idArray [1] = "Talk about <u>some activities you have been doing recently in your free time</u>, including the hobbies you've been doing, the food you've been eating, the movies/series you've been watching, the vide games you've been playing, etc."
+  idArray [2] = "<u>Describe your favorite sports, pastime activities or or hobbies you like doing</u>, including the activities, the frequency, the gear, the locations and people with whom you do them."
+  idArray [3] = "Talk about <u>the sport activity, exercise & hobbies you did when you were in high school</u>, including those you liked & didn't like so much, for how long you exercised, where you went to do them, etc."
+
+// UNIT 3:
+  idArray [4] = "Think about <u>someone you know well and describe this person</u>, including their physical appearance, their personality</u> and a little bit of their daily routine."
+  idArray [5] = "Talk about <u>someone you know very well & compare yourself to this person</u>, including their physical appearance, personality, hobbies, likes/dislikes, etc."
+  idArray [6] = "Think about two people in your family. <u>Describe and compare all their different characteristics</u>, including physical features, personalities and routines, etc… including how they are different from each other."
+  idArray [7] = "<u>Describe your favorite holiday celebration</u>. Talk about the event and the why you celebrate it, the things you do, if you visit a special locations, the food & drinks, the music, etc."
+  idArray [8] = "Talk about <u>the last time you had a party</u> or <u>a family celebration</u>, including what it was, when & where it was, what you did, what you ate, the clothes you were wearing, etc."
+  idArray [9] = "Talk about <u>how different your life is right now compared to when you were a child</u>, including your daily routine, hobbies, likes/dislikes, responsibilities, food & drink, clothing, etc."
+  idArray [10] = "Talk about <u>the differences between a summer vacation & a winter vacation</u>. Compare both seasons including the advantages or disadvantages of each one, the activities you can/can't do, the differences in the clothes you need, etc."
+
 // Peter's Questions:
-  idArray [5] = "Talk about some activities you have been doing recently in your free time."
-  idArray [6] = "Talk about a memorable holiday you have had."
-  idArray [7] = "What advice can you give to someone with a cold?"
-  idArray [8] = "Compare a beach holiday with a skiing holiday."
+  idArray [11] = "Talk about some activities you have been doing recently in your free time."
+  idArray [12] = "Talk about a memorable holiday you have had."
+  idArray [13] = "What advice can you give to someone with a cold?"
+  idArray [14] = "Compare a beach holiday with a skiing holiday."
 
 // SET THE NUMBER OF QUESTIONS ON THE SECOND LINE BELOW!
   document.getElementById("selectone").onclick = myFunctionOne;
-  randomParagraph = Math.floor(Math.random()*8);
+  randomParagraph = Math.floor(Math.random()*10);
 
 // THIS LINE DISPLAYS THE RANDOM QUESTIONS [DO NOT CHANGE]!
   document.getElementById("resultone").innerHTML = idArray[randomParagraph + 1];
--- questions/pairwork.js
+++ questions/pairwork.js
@@ -3,21 +3,25 @@
 // MAKE SURE TO START AND FINISH WITH QUOTATION SIGNS.
 function myFunctionPairs() {
   idArray = new Array()
+
   idArray [1] = "Talk to each other about <u>what you did last summer / last winter vacation</u>, including the activities you did, places you visited, people you met, movies you saw, books you read, videogames you played, etc."
-  idArray [2] = "<u>Talk to each other about your family</u>. Ask and answer questions about personal information, appearance, daily activities & routines, jobs or occupations, likes & dislikes, etc."
-  idArray [3] = "Talk to each other about <u>your social networking activities and routines</u>, including platforms, frequency, activities, friends & connections, etc."
+  idArray [2] = "Talk to each other about <u>the last time you went to the beach or a swimming pool</u>. Ask each other questions about the location you visited, the different activities you did, the people with whom you went there, what you ate there, etc."
+  idArray [3] = "Talk to each other about <u>all the things you did last year (2021/2022)</u>. Ask each other questions about the different activities you did, the places you visited, the people you met, the things you bought, what you ate, the movies or series you saw, etc."
   idArray [4] = "Discuss <u>some things you have never done, but would like to do in the future</u> & why. Mention some places you haven’t visited, sports you haven’t tried, things you haven’t learned, food you haven’t tasted, etc."
-  idArray [5] = "Talk to each other about <u>the celebrations you enjoy with your family</u>, including different event and the things you do, the places included, the food & drinks, the music, etc."
-  idArray [6] = "You were planning to have a small house party together, but none of your friends come. Discuss <u>what must/may have happened</u> as the possible reasons (problems, accidents, weather, other events, etc)."
+
+  idArray [5] = "<u>Talk to each other about your families</u>. Ask and answer questions about personal information, appearance, daily activities & routines, jobs or occupations, likes & dislikes, etc."
+  idArray [6] = "Talk to each other about <u>your social networking activities and routines</u>, including platforms, frequency, activities, friends & connections, etc."
+  idArray [7] = "Talk to each other about <u>the celebrations you enjoy with your family</u>, including different event and the things you do, the places included, the food & drinks, the music, etc."
+  idArray [8] = "You were planning to have a small house party together, but none of your friends come. Discuss <u>what must/may have happened</u> as the possible reasons (problems, accidents, weather, other events, etc)."
+
 // Peter's Tasks:
-  idArray [7] = "Talk to each other about the last time you went on holiday, including when / where this was, & who you were with. Take turns to ask / answer questions & follow-up questions."
-  idArray [8] = "Talk about some bad weather you experienced recently, including when this was, what you were doing when it started and how you felt. Take turns to ask / answer questions & follow-up questions."
+  idArray [7] = "Talk to each other about the last time you went on a long vacation, including when / where this was, & who you were with. Take turns to ask / answer questions & follow-up questions."
   idArray [9] = "Imagine you both don’t feel very well today. Tell each other exactly how you feel, and give each other some advice to get better."
   idArray [10] = "Discuss what type of holiday you prefer, a cultural holiday or a beach holiday. Compare them."
 
 // SET THE NUMBER OF QUESTIONS ON THE SECOND LINE BELOW!
   document.getElementById("selectpairs").onclick = myFunctionPairs;
-  randomParagraph = Math.floor(Math.random()*10);
+  randomParagraph = Math.floor(Math.random()*8);
 
 // THIS LINE DISPLAYS THE RANDOM QUESTIONS [DO NOT CHANGE]!
   document.getElementById("resultpairs").innerHTML = idArray[randomParagraph + 1];
