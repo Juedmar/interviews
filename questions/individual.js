a2afeec306d15548c8ea1f583b395aa06a0cfdd6
Juan Eduardo Méndez Arias <116532250+Juedmar@users.noreply.github.com>
2023-09-23 14:31:50 -0300

    Add files via upload

--- questions/individual.js
+++ questions/individual.js
@@ -3,25 +3,39 @@
 // MAKE SURE TO START AND FINISH WITH QUOTATION SIGNS.
 function myFunctionOne() {
   idArray = new Array()
-  idArray [1] = "Practice: What is your full name?"
-  idArray [2] = "Practice: How are you doing today?"
-  idArray [3] = "Practice: What is your favorite color?"
-  idArray [4] = "Practice: What is your e-mail address?"
-  idArray [5] = "Practice: What is your favorite season?"
-  idArray [6] = "Practice: How often do you go shopping?"
-  idArray [7] = "Practice: How do you spell your last name?"
-  idArray [8] = "Practice: What is your favorite food or drink?"
-  idArray [9] = "Practice: What is your favorite day of the week?"
-  idArray [10] = "Practice: What is your favorite month of the year?"
-  idArray [11] = "Practice: What time do you get up in the morning?"
-  idArray [12] = "Practice: What is your favorite sport? Do you practice often?"
-  idArray [13] = "Practice: What do you usually do during long weekeneds or holidays?"
-  idArray [14] = "Practice: Do you have any hobbies? When did you start doing them?"
-  idArray [15] = "Notice: These are practice questions only. There are no interviews today!"
+
+// BASIC PRACTICE
+
+  idArray [1] = "What’s in <u>your living room</u>? Describe what’s in it (1 minute)."
+  idArray [2] = "What do you like <u>to drink on special occasions</u>? Explain (1 minute)."
+  idArray [3] = "<u>How many people are there in your family</u>? Describe at least 2 of them."
+  idArray [4] = "Who is <u>your closest family member</u>? Why? Describe his/her (1 minute)."
+  idArray [5] = "Talk about <u>you when you were in elementary school</u>. Mention how your personality was, where you were then, and also how different life was back in those days."
+  idArray [6] = "Talk about <u>all the things you did on your last birthday</u>. Mention where you were, what you did to celebrate, what you ate, the people you talked to, etc."
+  idArray [7] = "Think of a movie that you know really well. Now, <u>talk about the main story</u> and mention everything important that happened to the characters and what they did."
+  idArray [8] = "Talk about <u>how different your life was before you started university</u>. Mention where you were, what your routine was, and anything that was different."
+  idArray [9] = "Talk about <u>all the things you did on your last Christmas</u> or </u>New Year's celebration</u>. Mention where you were, what you did, what you ate, the people you talked to, etc."
+  idArray [10] = "Think TV series / book that you know really well. Now, talk about <u>the main story and mention everything important that happened</u> to the characters and what they did."
+
+// INTERMEDIATE PRACTICE
+
+  idArray [11] = "<u>What's a typical Saturday for you?</u> Describe what you generally do and what you don't do on this day."
+  idArray [12] = "Talk about all <u>the things that you are doing this year</u>. Mention things about your family or personal life, your education, sports, music or hobbies, etc."
+  idArray [13] = "<u>What do you usually do on Friday nights?</u> Talk about what you usually do at the end of the week, including the activities, frequency, times, locations, etc."
+  idArray [14] = "Think about someone you know well and <u>describe this person, including their physical appearance, their personality</u> and a little bit of their daily routine."
+  idArray [15] = "<u>Describe your ideal romantic partner</u>. Talk about his/her appearance, personality, why you would like them and the things he or she normally does."
+  idArray [16] = "How many people are there in your family? <u>Describe at least 2 of them, including their physical appearance, their personality</u> and a little bit of their daily routine."
+  idArray [17] = "Who is your closest or favorite family member? Why? <u>Describe him/her in about 1 minute</u>, including some of the things they normally do."
+  idArray [18] = "Talk about any <u>hobbies, sports or healthy activities you have practiced</u>. Try to fully describe your experieces, including every detail."
+  idArray [19] = "Talk about <u>how you first met</u> one of your friends or classmates. You should say <u>when you met</u>, where you met, what you and your friend <u>were doing</u> at that time."
+  idArray [20] = "Talk about one activity using technology you <u>have been doing recently</u> (surfing the internet, watching TV, using social media etc). You should explain <u>why you have been using it</u>."
+  idArray [21] = "Talk about your <u>experiences of music you have had</u>; for example, types of music you have listened to, gigs or concert <u>you have been to or watched on TV</u>."
+  idArray [22] = "Talk about the <u>last time you went out</u> with a friend or family member. You should say <u>where you went, what you did</u> and why."
+  idArray [23] = "Talk about the <u>last time you went on holiday</u>. You should say <u>who you went with, where you went, what you did</u> and if you enjoyed it."
 
 // SET THE NUMBER OF QUESTIONS ON THE SECOND LINE BELOW!
   document.getElementById("selectone").onclick = myFunctionOne;
-  randomParagraph = Math.floor(Math.random()*15);
+  randomParagraph = Math.floor(Math.random()*23);
 
 // THIS LINE DISPLAYS THE RANDOM QUESTIONS [DO NOT CHANGE]!
   document.getElementById("resultone").innerHTML = idArray[randomParagraph + 1];
--- questions/pairwork.js
+++ questions/pairwork.js
@@ -3,17 +3,53 @@
 // MAKE SURE TO START AND FINISH WITH QUOTATION SIGNS.
 function myFunctionPairs() {
   idArray = new Array()
-  idArray [1] = "Practice: Talk to each other about your favorite foods and drinks."
-  idArray [2] = "Practice: Talk to each other about your daily routine and pastimes."
-  idArray [3] = "Practice: Talk to each other about about your ideal vacation."
-  idArray [4] = "Notice: These are practice questions only. There are no interviews today!"
+
+// BASIC PRACTICE
+
+  idArray [1] = "Your work together for the first time. <u>Have a conversation to get to know each other</u>."
+  idArray [2] = "Talk to each other <u>about your family</u>. Ask about information, appearance, daily activities, etc."
+  idArray [3] = "You & your partner want to start exercising. Ask each other about <u>your activities in the week to find a time to go to the gym together</u>."
+  idArray [4] = "You would like <u>to know each other’s best friend</u>. Have a conversation about them."
+  idArray [5] = "Talk to each other <u>about your favorite drinks and food</u>."
+  idArray [6] = "Have a conversation <u>about your diet</u>. Are you healthy?"
+  idArray [7] = "Have a conversation <u>about each other's kitchens</u>."
+  idArray [8] = "Talk about the <u>advantages and disadvantages of drinking water</u>."
+  idArray [9] = "Have a conversation answering the next question: <u>What are the most popular kinds of restaurants in Chile</u>?"
+  idArray [10] = "You are going to have <u>a party for the class. Decide what food/drinks you need</u>."
+  idArray [11] = "Talk about <u>your future and make predictions about what will/might happen</u> in your personal life."
+  idArray [12] = "Talk about <u>your future and make predictions about what will/might happen</u> in your family life."
+  idArray [13] = "Describe <u>a job you would love doing</u>. Say why and explain what you must/have to do and the things you can/can’t do."
+  idArray [14] = "Describe <u>a job you would hate doing</u>. Say why and explain what you must/have to do and the things you can/can’t do."
+  idArray [15] = "Discuss <u>all the things you would need to take if you go on a trip to the desert</u> for a week with a friend."
+  idArray [16] = "Discuss <u>all the things you would need to prepare if you go on a trip to the desert</u> for a week with a friend."
+
+// INTERMEDIATE PRACTICE
+
+  idArray [17] = "Discuss how <u>our grandparents' lives were, including how much easier / harder some things were</u> about 50 - 80 years ago."
+  idArray [18] = "Have a conversation with your partner about <u>the things you do well and things you aren’t good at</u>, give examples and ask each other questions."
+  idArray [19] = "Talk to your partner about all the things <u>you didn’t do this year</u>. Mention any reasons, including what you still can’t / couldn’t do and why. Remember to ask questions."
+  idArray [20] = "Talk to your partner about <u>any interesting activities they did / have done lately</u>. Think about last weekend, last month or their last vacation. Ask and answer questions."
+  idArray [21] = "Discuss what is <u>the best / worst job you know</u>. Talk about all the good and bad things, advantages, what you can/can’t do, etc. Mention if you would do it (or not), and why."
+
+  idArray [22] = "Talk to each other <u>about the daily routine as a university student</u>, including the activities, times, frequency, meals, hobbies, family, transportation, etc. Take turns to ask / answer questions."
+  idArray [23] = "Talk to each other <u>about the daily routine you had when you were in high school</u>. Describe the times, frequency, activities, meals, hobbies, etc. Take turns to ask / answer questions."
+  idArray [24] = "You want to go to start exercising. Ask each other about <u>your activities and routines during the week to find a time to meet<u> at the gym."
+  idArray [25] = "Talk to each other <u>about your family. Ask and answer questions</u> about personal information, appearance, daily activities & routines, hobbies, etc."
+  idArray [26] = "Talk to each other <u>about your social networking activities and routines</u>, including platforms, frequency, activities, friends & connections, etc."
+  idArray [27] = "Talk to each other <u>about sport and hobbies people like to practice regularly</u>, including the activities, the frequency, competitions, teams, etc. Exchange personal experiences."
+  idArray [28] = "Discuss <u>the hobbies and sports younger people like to practice</u>, including the activities, the frequency, the gear and locations, etc. Askeach other about your personal experiences."
+  idArray [29] = "Talk to each other about <u>different types of technology you have been using recently</u>, including how and why you have been using them. Take turns to ask / answer questions, including follow-up questions."
+  idArray [30] = "Talk to each other about your<u> knowledge and experiences of your favourite[🇬🇧] music groups or singers</u>, including the ones you have heard of and how you have consumed their music (on YouTube, at a concert etc)."
+  idArray [31] = "Talk to each other about <u>the last time you played sport or did some physical exercise</u>, including when, where, and who you were with. Take turns to ask / answer questions, including follow-up questions."
+  idArray [32] = "Talk about <u>some good news you received </u>(you passed an exam, you won a prize, or a family member had a baby etc), including when this was, what you were doing when you received the news and how you felt."
+  idArray [33] = "Talk about <u>the last time you celebrated something (someone’s birthday, a wedding etc)</u>, including when, where, and who was there. Take turns to ask / answer questions, including follow-up questions."
 
 // SET THE NUMBER OF QUESTIONS ON THE SECOND LINE BELOW!
   document.getElementById("selectpairs").onclick = myFunctionPairs;
-  randomParagraph = Math.floor(Math.random()*4);
+  randomParagraph = Math.floor(Math.random()*33);
 
 // THIS LINE DISPLAYS THE RANDOM QUESTIONS [DO NOT CHANGE]!
-  document.getElementById("resultpairs").innerHTML = idArray[randomParagraph + 1];       
+  document.getElementById("resultpairs").innerHTML = idArray[randomParagraph + 1];
 
 // THIS LINE HIDES THE TEXT OF INDIVIDUAL TOPICS [DO NOT CHANGE]!
   document.getElementById('resultone').style.color = '#FFFF44';
