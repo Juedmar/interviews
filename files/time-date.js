54e681f4bc7d262cdc9b8564f026bc29cd522a6c
Juan Eduardo Méndez Arias <116532250+Juedmar@users.noreply.github.com>
2025-06-27 15:36:17 -0400

    Delete files directory

--- files/time-date.js
+++
@@ -1,9 +0,0 @@
-
-const d = new Date();
-function refreshTime() {
-  const timeDisplay = document.getElementById("time");
-  const dateString = new Date().toLocaleString(); d.toDateString();
-  const formattedString = dateString.replace(", ", " - ⏰ Time: ");
-  timeDisplay.textContent = formattedString;
-}
-  setInterval(refreshTime, 1000);
